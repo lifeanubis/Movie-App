72a48e68ec82cf0f036a777059d54d51985c02ce
shreyansh <code.shreyansh@gmail.com>
2020-07-11 18:08:04 +0530

    code cleaned

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
 
-import React, { useState } from "react";
-import Search from "./movie/Search";
-import Axios from "axios";
-import Results from "./movie/Results";
+import React from "react";
 import MovieMain from "./movie/MovieMain";
-import Crud from "./crud/Crud";
+
 // import Hooku from "./Hooku";
 
 function App() {
