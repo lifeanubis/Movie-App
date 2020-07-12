ce7e61de6abeab82d0782ec87dd30a1979143408
shreyansh <code.shreyansh@gmail.com>
2020-07-12 10:59:22 +0530

    css added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
-// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Router>
@@ -13,4 +13,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// serviceWorker.unregister();
+serviceWorker.unregister();
